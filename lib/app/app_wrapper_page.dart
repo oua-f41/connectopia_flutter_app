1488523d01b71da8b76eb6b0b82bc6717a45ad2c
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-07-10 01:14:43 +0300

    update app wrapper

--- lib/app/app_wrapper_page.dart
+++ lib/app/app_wrapper_page.dart
@@ -1,19 +1,22 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:connectopia/features/maps/presentation/cubit/maps_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../features/category/presentation/cubit/category_cubit.dart';
 import '../product/widgets/global_progress_loading_bar.dart';
 import 'connectopia_app_cubit.dart';
 
 @RoutePage()
-class AppWrapperPage extends StatelessWidget {
+class AppWrapperPage extends StatelessWidget with AutoRouteWrapper {
   const AppWrapperPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        const AutoRouter(),
+        HeroControllerScope(
+            controller: HeroController(), child: const AutoRouter()),
         BlocBuilder<ConnectopiaAppCubit, ConnectopiaAppViewModel>(
           builder: (context, state) {
             return state.isLoading
@@ -24,6 +27,16 @@ class AppWrapperPage extends StatelessWidget {
       ],
     );
   }
-}
 
-/* */
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider(
+        create: (ctx) => CategoryCubit()..init(),
+      ),
+      BlocProvider(
+        create: (ctx) => MapsCubit()..init(),
+      ),
+    ], child: this);
+  }
+}
