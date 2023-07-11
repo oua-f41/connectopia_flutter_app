535233c4c68bbf4e8d9c2260ab7d15d4b3d44746
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-07-11 16:02:08 +0300

    add donate feature

---
+++ lib/features/donate/presentation/cubit/donate_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:f41/features/donate/presentation/cubit/view_model/donate_view_model.dart';
+
+import '../../../../app/base_cubit.dart';
+
+class DonateCubit extends BaseCubit<DonateViewModel>{
+  DonateCubit() : super(DonateViewModel());
+
+  void selectAmount(int amount) {
+    emit(state.copyWith(selectedAmount: amount));
+  }
+  
+}
